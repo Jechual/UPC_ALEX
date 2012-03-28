8da8ee85ba99dd1dcc1b8f9cf41febaf7e99778e
Jesus Segovia <jesusitos@yahoo.es>
2012-03-28 15:41:51 -0500

    borrar

--- ConejoColita/src/conejocolita/ConejoColita.java
+++ ConejoColita/src/conejocolita/ConejoColita.java
@@ -1,18 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package conejocolita;
 
-/**
- *
- * @author pcsidsub
- */
+
 public class ConejoColita {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         // TODO code application logic here
     }
